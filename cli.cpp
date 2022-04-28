ad43d2678de822278c534c19407926b9e492cbf6
Izzy Ehnes <izzyehnes@gmail.com>
2022-04-27 21:22:28 -0700

    Add documentation

--- cli.cpp
+++ cli.cpp
@@ -1,13 +1,27 @@
+/** 
+ * AUTHOR: Izzy Ehnes
+ * DATE: 27 April 2022
+ * DESCRIPTION: A command line interpreter that allows users to execute certain Unix commands via a simple shell.
+ **/
+
+
 #include <iostream>
 #include <queue>
 
 using namespace std;
 
+
+
+// List of predefined commands
 string legal_commands[14] = {"cd", "exec", "exit", "gcc", "ls", "man", "more", "mv", "rm", "pwd", "sh", "touch", "which", "$path"};
+
 int legal_commands_length = sizeof(legal_commands) / sizeof(int);
 
 
 
+/** 
+ * Prints all of the predefined legal commands
+ **/
 void print_legal_commands()
 {
 
@@ -21,15 +35,26 @@ void print_legal_commands()
 }
 
 
+
+/** 
+ * Prints the author, release date, as well as a list of legal commands.
+ **/
 void print_greeting()
 {
     cout << "CLI by Izzy Ehnes\n";
     cout << "Released 27 April 2022\n";
     print_legal_commands();
+    cout << "Multiple arguments of one cli command must be separated from one another by commas.\nPossible parameters of any one command are separated from the command itself (and from possible further parameters) by white space";
+    cout << "\nExample: \"pwd, ls -la\"\n\n";
 }
 
 
 
+/** 
+ * Returns only the command (and no arguments) in inString.
+ * 
+ * E.g. extract_command("pwd") will return "pwd", and extract_command("ls -la") will return "ls".
+ **/
 string extract_command(string inString)
 {
     string command = "";
@@ -55,6 +80,9 @@ string extract_command(string inString)
 
 
 
+/** 
+ * Returns 'true' if inCommand is in legal_commands and false otherwise.
+ **/
 bool is_legal_command(string inCommand)
 {
     for (int i = 0; i < legal_commands_length; i++)
@@ -70,6 +98,9 @@ bool is_legal_command(string inCommand)
 
 
 
+/** 
+ * Executes an individual command with its arguments, if there are any
+ **/
 void execute_command(const char* command)
 {
     system(command);
@@ -77,10 +108,14 @@ void execute_command(const char* command)
 
 
 
+/** 
+ * Processes the queue of commands, and executes them if they are legal.
+ **/
 void process_queue(std::queue<string> in_queue)
 {
     cout << in_queue.size() + 1 << " strings passed to argv[]\n\n";
 
+    // While there are still commands in the queue
     while (!in_queue.empty())
     {
         string command = in_queue.front();
@@ -89,11 +124,13 @@ void process_queue(std::queue<string> in_queue)
 
         cout << "Next string is '" << command << "'\n";
 
+        // If the command is not one of the predefined commands, alert the user and then skip it and continue
         if (!is_legal_command(command_without_args))
         {
             cout << "'" << command_without_args << "' is not one of the predefined commands. Skipping...\n";
         }
 
+        // If the command is legal, execute it
         else
         {
             cout << "'" << command_without_args << "' is one of the predefined commands. Executing now...\n";
@@ -101,10 +138,12 @@ void process_queue(std::queue<string> in_queue)
             cout << endl;
         }
 
+        // The command at the front of the queue has been processed, so remove it
         in_queue.pop();
     
     }
 
+    // Prints the working directory once all commands have been executed (or skipped if they are illegal)
     execute_command("pwd");
     cout << "is the current working directory.";
     cout << "\n\nGoodbye.\n";
@@ -112,6 +151,9 @@ void process_queue(std::queue<string> in_queue)
 
 
 
+/**
+ * Processes the arguments passed in main()
+ **/
 void process_input(int argc, char *argv[], char* envp[])
 {
     // A queue that will hold all of our commands 
@@ -155,6 +197,7 @@ void process_input(int argc, char *argv[], char* envp[])
         }
     }
 
+    // All commands and any associated arguments have been queued, so command_queue is ready to be processed
     process_queue(command_queue);
 }
 
