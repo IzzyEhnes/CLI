02f6c4f75d8d01066ece00e09f5a3fcc8bdb6936
Izzy Ehnes <izzyehnes@gmail.com>
2022-04-27 18:51:06 -0700

    Add helper functions

--- cli.cpp
+++ cli.cpp
@@ -2,10 +2,50 @@
 using namespace std;
 
 string legal_commands[14] = {"cd", "exec", "exit", "gcc", "ls", "man", "more", "mv", "rm", "pwd", "sh", "touch", "which", "$path"};
+int legal_commands_length = sizeof(legal_commands) / sizeof(int);
+
+
+
+void print_legal_commands()
+{
+
+    cout << "Legal commands: ";
+    for (int i = 0; i < legal_commands_length; i++)
+    {
+        cout << legal_commands[i] << ' ';
+    }
+
+    cout << endl;
+}
+
+
+
+bool is_legal_command(string inCommand)
+{
+    for (int i = 0; i < legal_commands_length; i++)
+    {
+        if (legal_commands[i] == inCommand)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+
+void execute_CLI(const char* command)
+{
+    system(command);
+}
+
+
 
 void get_input(int argc, char *argv[], char* envp[])
 {
-    cout << "argc = " << argc << endl;
+    print_legal_commands();
+
     for (int i = 1; i < argc; i++)
     {
         cout << "argv[" << i << "] = " << argv[i] << endl;
@@ -15,12 +55,9 @@ void get_input(int argc, char *argv[], char* envp[])
     }
 }
 
-void execute_CLI(const char* command)
-{
-    system(command);
-}
+
 
 int main(int argc, char *argv[], char* envp[])
 {
-    get_input(argc, *argv[], envp[]);
+    get_input(argc, argv, envp);
 }
